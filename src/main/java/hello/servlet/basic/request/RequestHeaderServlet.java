976fc858ccdf9de23f24a6c8a642a18a10025721
coollpice <coollpice@naver.com>
2021-08-09 21:15:18 +0900

    서블릿 Init

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -1,18 +1,35 @@
 package hello.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import org.springframework.util.StreamUtils;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
-@WebServlet(name = "requestHeaderServlet" , urlPatterns = "/request-header")
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        ServletInputStream inputStream = req.getInputStream();
+        String str = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        HelloData helloData = objectMapper.readValue(str, HelloData.class);
+
+        System.out.println("name = " + helloData.getName());
+        System.out.println("age = " + helloData.getAge());
+
+        resp.getWriter().write("SUCCESS");
 
     }
 
