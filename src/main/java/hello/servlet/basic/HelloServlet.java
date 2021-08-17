b102537102a0b794284fe5f79117e34bae60a0a2
coollpice <coollpice@nvaer.com>
2021-08-17 18:19:33 +0900

    스프링 MVC 1

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet" , urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,23 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    private Map<String, Object> paramMap = new HashMap<>();
+
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
